2de1ad204822b56fc2b2fc709af1dd5f10c4a1ca
William Reilly <reilly.williamc@gmail.com>
2023-03-20 20:09:15 -0400

    set up, get computer choice, get player choice, and sucefully output results to the console

--- java.js
+++ java.js
@@ -4,6 +4,8 @@ const playerSelection = getPlayerSelection("roCK");
 const roundResult = singleRound(computerChoice,playerSelection);
 console.log(computerChoice + " "+ playerSelection +"\n"+roundResult);
 
+
+
 function getComputerChoice(){;//returns random rock/paper/scissors
     const a= Math.floor(Math.random() *3)+1;
     let choice;
@@ -21,11 +23,19 @@ function getPlayerSelection(playerInput){//player chooses rock/paper/scissors fr
 return playerInput.toLowerCase();
 }
 function singleRound(comp,play){ //takes playerSelection and getComputerChoice and outputs text
-if(comp === play){
-    return "tie game!"
-}else if(comp === "rock" && play === "paper" || comp === "paper" && play === "scissors" || comp === "scissors" && play === "rock"){
-    return play+ " beats " +comp+". you win!!"
-}else{
-    return comp+ " beats " +play+". you loose!"
+    if(comp === play){
+        return "tie game!"
+    }else if(comp === "rock" && play === "paper" || comp === "paper" && play === "scissors" || comp === "scissors" && play === "rock"){
+        return play+ " beats " +comp+". you win!!"
+    }else{
+        return comp+ " beats " +play+". you loose!"
+    }
 }
+
+function play(){
+    for(const i=1; i<=5; i++){
+        
+    }
+
+
 }
